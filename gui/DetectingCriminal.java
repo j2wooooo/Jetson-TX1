aceafd0ce3aa49b0e28ecf0fd5cf6f1d7a2d9299
hwayoungBaek <wwwo3owww@naver.com>
2018-10-25 10:50:10 +0900

    Update DetectingCriminal.java

--- gui/DetectingCriminal.java
+++ gui/DetectingCriminal.java
@@ -3,13 +3,19 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
+import java.awt.event.KeyListener;
+import java.awt.Dimension;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 
 public class DetectingCriminal extends JFrame implements ActionListener{
@@ -18,11 +24,12 @@ public class DetectingCriminal extends JFrame implements ActionListener{
 	                         //                     = 2,  if hayoung
 	private static String detection_date_time;
 	private static String detection_accuracy;
+	private static Dimension dimen;
 	
 	public DetectingCriminal() {
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(1300,600);
+		this.setUndecorated(true); // full screen
+		setSize((int)dimen.width,(int)dimen.height); // size
 		setTitle("Detected Criminal");
 		
 		setLayout(null);
@@ -57,7 +64,8 @@ public class DetectingCriminal extends JFrame implements ActionListener{
 		label_user.setBounds(650, 380,500,100);
 		label_user.setFont(new Font("Serif",Font.ITALIC,20));
 		
-		JLabel label_accuracy = new JLabel("Index of Similarity : "+detection_accuracy +"% \n");	
+		//JLabel label_accuracy = new JLabel("Index of Similarity : "+detection_accuracy +"% \n");	
+		JLabel label_accuracy = new JLabel("Index of Similarity : 0% \n");
 		label_accuracy.setBounds(130, 430,600,100);
 		label_accuracy.setFont(new Font("Serif",Font.ITALIC,50));
 		label_accuracy.setForeground(Color.RED);
@@ -69,7 +77,33 @@ public class DetectingCriminal extends JFrame implements ActionListener{
 		this.add(criminal_ImgBox);
 		this.add(usr_ImgBox);
 		
+		this.addKeyListener(new MyKeyListener());
+		
 		setVisible(true);
+		
+		//refresh frame
+		int accuracy = Integer.parseInt(detection_accuracy);
+		for(int i=0; i<=accuracy; i++) {
+			label_accuracy.setText("Index of Similarity : "+i +"% \n");
+			SwingUtilities.updateComponentTreeUI(this);
+			try {
+				Thread.sleep(18);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	class MyKeyListener extends KeyAdapter{
+		public void keyPressed(KeyEvent e) {
+			System.out.println("pressed Key : "+e.getKeyCode());
+		
+			if(e.getKeyCode()==27) { // end
+				System.exit(0);
+			}
+		}
 	}
 	
 	
@@ -88,6 +122,8 @@ public class DetectingCriminal extends JFrame implements ActionListener{
 		detection_date_time = "2018-10-24xxxx";// args[1]
 		detection_accuracy = "94"; // args[2]
 		
+		dimen = Toolkit.getDefaultToolkit().getScreenSize();
+		
 		DetectingCriminal av=new DetectingCriminal();
 		
 	}
@@ -99,3 +135,5 @@ public class DetectingCriminal extends JFrame implements ActionListener{
 		
 	}
 }
+
+
